69273f485bb3d3222970fa0db2aa7b8ca0b2c433
Takashi Mizohata <beatak@gmail.com>
2012-09-06 12:45:05 -0400

    fixes broken test cases

--- test/privates.text.js
+++ test/privates.text.js
@@ -1,7 +1,7 @@
 var p = require('../lib/eachhtmlchar.js'),
 getNext = p._getNext,
-getEntity = p._getEntity,
-getTag = p._getTag,
+getEntity = p._getFirstAppearingEntity,
+getTag = p._getFirstAppearingTag,
 assert = require('assert');
 
 module.exports = {
@@ -11,15 +11,19 @@ module.exports = {
   },
 
   'getEntity: 0': function () {
-    assert.deepEqual([0, '&nbsp;'], getEntity('&nbsp;'));
+    assert.equal('&nbsp;', getEntity('&nbsp;'));
   },
 
   'getEntity: 1': function () {
-    assert.deepEqual([1, '&NbSp;'], getEntity('a&NbSp;'));
+    assert.equal('&NbSp;', getEntity('a&NbSp;'));
   },
 
   'getEntity: 2': function () {
-    assert.deepEqual([2, '&#8482;'], getEntity('01&#8482;34'));
+    assert.equal('&#8482;', getEntity('01&#8482;34'));
+  },
+
+  'getEntity: 3': function () {
+    assert.equal(null, getEntity('01& #8482 ;34'));
   },
 
   'getTag: null': function () {
@@ -27,27 +31,27 @@ module.exports = {
   },
 
   'getTag: 0': function () {
-    assert.deepEqual([0, '<foo>'], getTag('<foo>bar'));
+    assert.equal('<foo>', getTag('<foo>bar'));
   },
 
   'getTag: 1': function () {
-    assert.deepEqual([1, '<FoO>'], getTag('0<FoO>bar'));
+    assert.equal('<FoO>', getTag('0<FoO>bar'));
   },
 
   'getTag: 2': function () {
-    assert.deepEqual([0, '<foo/>'], getTag('<foo/>'));
+    assert.equal('<foo/>', getTag('<foo/>'));
   },
 
   'getTag: 3': function () {
-    assert.deepEqual([0, '<foo />'], getTag('<foo />'));
+    assert.equal('<foo />', getTag('<foo />'));
   },
 
   'getTag: 4': function () {
-    assert.deepEqual([0, '<foo a="0" b=\'1\'>'], getTag('<foo a="0" b=\'1\'>'));
+    assert.equal('<foo a="0" b=\'1\'>', getTag('<foo a="0" b=\'1\'>'));
   },
 
-  'getTag: ': function () {
-    assert.deepEqual([0, '<foo a="<bar bar>">'], getTag('<foo a="<bar bar>">bar</foo>'));
+  'getTag: 5': function () {
+    assert.deepEqual('<foo a="<bar bar>">', getTag('<foo a="<bar bar>">bar</foo>'));
   },
 
 /*
@@ -57,30 +61,30 @@ module.exports = {
 */
 
   'getNext: 0': function () {
-    assert.equal('b', getNext(['abc', 1], p.COUNT_AS_ONE, p.COUNT_AS_ONE));
+    assert.deepEqual({word: 'a', incr: 1}, getNext('abc', p.COUNT_AS_ONE, p.COUNT_AS_ONE));
   },
 
   'getNext: 1': function () {
     var str = 'ab<br/>0';
     var expec = ['a', 'b', '<br/>', '0'];
-    var ii = 0, word;
+    var ii = 0, next;
     for (var i = 0, len = expec.length; i < len; ++i) {
-      word = getNext({0:str, 1:ii}, p.COUNT_AS_ONE, p.COUNT_AS_ONE);
+      next = getNext(str.slice(ii), p.COUNT_AS_ONE, p.COUNT_AS_ONE);
       // console.log(i, ii, expec[i], word);
-      assert.equal(expec[i], word);
-      ii += word.length;
+      assert.equal(expec[i], next.word);
+      ii += next.incr;
     }    
   },
 
   'getNext: 2': function () {
     var str = 'ab&foo;0';
     var expec = ['a', 'b', '&foo;', '0'];
-    var ii = 0, word;
+    var ii = 0, next;
     for (var i = 0, len = expec.length; i < len; ++i) {
-      word = getNext({0:str, 1:ii}, p.COUNT_AS_ONE, p.COUNT_AS_ONE);
+      next = getNext(str.slice(ii), p.COUNT_AS_ONE, p.COUNT_AS_ONE);
       // console.log(i, ii, expec[i], word);
-      assert.equal(expec[i], word);
-      ii += word.length;
+      assert.equal(expec[i], next.word);
+      ii += next.incr;
     }    
   }
 
